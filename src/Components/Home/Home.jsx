97feb182aaa7b9161ab32fbe48a724f61362ef9d
antor-chandra-ray <rantorchandra@gmail.com>
2026-03-01 10:05:59 +0600

    remove user id

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Home = ({ data }) => {
   // Add safety check - if data doesn't exist, return null or loading message
   if (!data) {
-    return <div>Loading post...</div>;
+    return;
   }
 
-  const { body, title, id, userId } = data;
+  const { body, title, id } = data;
 
   return (
     <div
@@ -20,7 +20,6 @@ const Home = ({ data }) => {
       }}
     >
       <h3 style={{ color: "#333", marginTop: 0 }}>Post #{id}</h3>
-      <p style={{ color: "#666", fontSize: "14px" }}>User ID: {userId}</p>
       <h4 style={{ color: "#0066cc" }}>{title}</h4>
       <p style={{ color: "#555", lineHeight: "1.6" }}>{body}</p>
     </div>
